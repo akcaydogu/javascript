d0ffc54a0865e93f4d996b3f49a8928f7e9a7551
Doğukan Akçay <akcay.dogukan61@gmail.com>
2025-04-26 19:49:12 +0300

    Objects

---
+++ scripts/08-todo-list.js
@@ -0,0 +1,57 @@
+const todoList = [
+    {
+        name: 'dinner',
+        dueDate: '2022-12-22'
+    },
+    {
+        name: 'help',
+        dueDate: '2022-5-22'
+    }
+];
+
+function addTodo(){
+    const inputElement = document.querySelector('#name-input')
+    const name = inputElement.value;
+    const dateElement = document.querySelector('#date-input')
+    const dueDate = dateElement.value;
+    todoList.push({
+        name: name,
+        dueDate: dueDate
+    })
+    inputElement.value = '';
+    updateTodoList()
+    console.log(todoList)
+}
+
+function updateTodoList(){
+    let todoListHtml = ''
+    todoList.forEach((todoObject, index) => {
+        const { name, dueDate} = todoObject
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+            todoList.splice(${index}, 1);
+            updateTodoList();
+        " class="delete-todo-btn">Delete</button>
+        `
+        todoListHtml += html;
+    })
+    /*for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        const { name, dueDate} = todoObject
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+            todoList.splice(${i}, 1);
+            updateTodoList();
+        " class="delete-todo-btn">Delete</button>
+        `
+        todoListHtml += html;
+    }*/
+    document.querySelector('#todo-list')
+    .innerHTML = todoListHtml;
+}
